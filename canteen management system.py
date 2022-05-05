ed3eefaea41cde870d860014616d289a5492cfd8
Lona Matshingana <71089848+Lona-web558@users.noreply.github.com>
2022-05-05 15:20:01 +0200

    Add files via upload

---
+++ canteen management system.py
@@ -0,0 +1,117 @@
+﻿//====================================================================================================
+//The Free Edition of C++ to Python Converter limits conversion output to 100 lines per file.
+
+//To purchase the Premium Edition, visit our website:
+//https://www.tangiblesoftwaresolutions.com/order/order-cplus-to-python.html
+//====================================================================================================
+
+#C++ TO PYTHON CONVERTER WARNING: The following #include directive was ignored:
+##include<process.h>
+
+def main():
+    clrscr = 0
+    sum = None
+    add = None
+    d = None
+    i = None
+    j = None
+    e = None
+    f = None
+    g = None
+    h = None
+    k = None
+    l = None
+    w = None
+    x = None
+    y = None
+    z = None
+    bir = 10
+    per = 36
+    der = 110
+    mur = 8
+    par = 110
+    cor = 90
+    twr = 1199
+    lur = 17
+    tir = 107
+    rer = 1499
+    qubis = 1000
+    abis = 0
+    qupep = 1000
+    apep = 0
+    quden = 110
+    aden = 0
+    qumun = 1000
+    amun = 0
+    quperk = 500
+    aperk = 0
+    qucoc = 400
+    acoc = 0
+    qutit = 100
+    atit = 0
+    qulux = 1000
+    alux = 0
+    qutid = 1000
+    atid = 0
+    quree = 300
+    aree = 0
+    m = 0
+    n = 0
+    o = 0
+    p = 0
+    q = 0
+    r = 0
+    s = 0
+    t = 0
+    u = 0
+    v = 0
+    name = str(['\0' for _ in range(50)])
+    rank = str(['\0' for _ in range(10)])
+    pu = None
+    c = str(['\0' for _ in range(20)])
+    a = str(['\0' for _ in range(15)])
+    b = str(['\0' for _ in range(15)])
+    ok = None
+    clrscr()
+    printf("\n +++++++++++++++++++++++++++++++++++++++*++++++++++++++++++++++++++++++++++++++")
+    printf("\n +                                     ***                                    +")
+    printf("\n +                                    *****                                   +")
+    printf("\n +                                   *******                                  +")
+    printf("\n +                                  *********                                 +")
+    printf("\n +                                 ***********                                +")
+    printf("\n +                                *************                               +")
+    printf("\n +                               ***************                              +")
+    printf("\n +                              *****************                             +")
+    printf("\n +                             *******************                            +")
+    printf("\n +                            *********************                           +")
+    printf("\n +                           ***********************                          +")
+    printf("\n +                          *************************                         +")
+    printf("\n +                         ***************************                        +")
+    printf("\n +                        *****************************                       +")
+    printf("\n +                       *******************************                      +")
+    printf("\n +                      *********************************                     +")
+    printf("\n +                     ***********************************                    +")
+    printf("\n +                    *************************************                   +")
+    printf("\n +                   ***************************************                  +")
+    printf("\n +                  *****************************************                 +")
+    printf("\n +                 *******************************************                +")
+    printf("\n +                *********************************************               +")
+    printf("\n +               ***********************************************              +")
+    printf("\n +              *************************************************             +")
+    printf("\n +             *********************WELCOME***********************            +")
+    printf("\n +              *************************************************             +")
+    printf("\n +               ***********************************************              +")
+    printf("\n +                *********************************************               +")
+    printf("\n +                 *******************************************                +")
+    printf("\n +                   ***************************************                  +")
+    printf("\n +                    *************************************                   +")
+    printf("\n +                     ***********************************                    +")
+    printf("\n +                      *********************************                     +")
+    printf("\n +                       *******************************                      +")
+
+//====================================================================================================
+//End of the allowed output for the Free Edition of C++ to Python Converter.
+
+//To purchase the Premium Edition, visit our website:
+//https://www.tangiblesoftwaresolutions.com/order/order-cplus-to-python.html
+//====================================================================================================
